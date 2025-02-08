70f801151b400cd91e92d7d56b0b70c50101d527
Marcelo Amaral Alves <marceloaa@live.com>
2025-02-08 13:01:38 -0500

    new user and new address

--- app.py
+++ app.py
@@ -1,45 +1,69 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class Addresses(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String(120), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
+# Modelo de Usuários (Agora inclui endereço principal)
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)  
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
 
-    # 🔹 Relacionamento: Um usuário pode ter vários endereços
+    # 🔹 Endereço principal do usuário
+    street = db.Column(db.String(120), nullable=False)
+    number = db.Column(db.String(20), nullable=False)
+    complement = db.Column(db.String(120))
+    zip_code = db.Column(db.String(20), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+
+    # 🔹 Relacionamento: Um usuário pode ter vários endereços extras
     addresses = db.relationship('Addresses', backref='user', lazy=True)
 
-# CRUD - READ (Exibir usuários e endereços)
+# Modelo de Endereços Adicionais
+class Addresses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    street = db.Column(db.String(120), nullable=False)
+    number = db.Column(db.String(20), nullable=False)
+    complement = db.Column(db.String(120))
+    zip_code = db.Column(db.String(20), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+# CRUD - READ (Listar usuários e endereços)
 @app.route('/')
 def index():
     users = Users.query.all()
     return render_template('index.html', users=users)
 
-# CRUD - CREATE (Criar novo usuário e endereço inicial)
+# CRUD - CREATE (Criar novo usuário com endereço principal)
 @app.route('/new_user', methods=['POST'])
 def create_users():
     new_user = request.form['name']
     new_email = request.form['email']
     new_password = request.form['password']
-    new_address = request.form['address']
-    
-    user = Users(name=new_user, email=new_email, password=new_password)
-    db.session.add(user)
-    db.session.commit()
 
-    # Criar endereço inicial associado ao usuário
-    address = Addresses(address=new_address, user_id=user.id)
-    db.session.add(address)
+    # 🔹 Endereço principal do usuário
+    street = request.form['street']
+    number = request.form['number']
+    complement = request.form['complement']
+    zip_code = request.form['zip_code']
+    city = request.form['city']
+    state = request.form['state']
+    
+    # Criar usuário com endereço principal
+    user = Users(
+        name=new_user, email=new_email, password=new_password,
+        street=street, number=number, complement=complement,
+        zip_code=zip_code, city=city, state=state
+    )
+    db.session.add(user)
     db.session.commit()
     
     return redirect('/')
@@ -47,13 +71,22 @@ def create_users():
 # CRUD - CREATE (Adicionar novo endereço para um usuário existente)
 @app.route('/new_address', methods=['POST'])
 def create_address():
-    new_address = request.form['address']
-    user_name = request.form['name']  
-
+    user_name = request.form['name']  # O usuário é identificado pelo nome
     user = Users.query.filter_by(name=user_name).first()
     
     if user:
-        address = Addresses(address=new_address, user_id=user.id)
+        street = request.form['street']
+        number = request.form['number']
+        complement = request.form['complement']
+        zip_code = request.form['zip_code']
+        city = request.form['city']
+        state = request.form['state']
+
+        # Criar novo endereço para o usuário
+        address = Addresses(
+            street=street, number=number, complement=complement,
+            zip_code=zip_code, city=city, state=state, user_id=user.id
+        )
         db.session.add(address)
         db.session.commit()
     
